33df2cabfa64045a59e49d17b0c0bc5bac053073
Uday Sai Jagannadh Nandipati <sain@microsoft.com>
2021-10-14 18:50:29 +0530

    first improvements

--- src/configure.js
+++ src/configure.js
@@ -1,75 +1,77 @@
-'use strict';
-const electron = require('electron')
-const path = require('path')
-const remote = electron.remote
-const startBtn = document.getElementById('startbtn')
-const ioHook = require('iohook');
+"use strict";
+const electron = require("electron");
+const path = require("path");
+const remote = electron.remote;
+const startBtn = document.getElementById("startbtn");
+const ioHook = require("iohook");
 //const { remote } = require('electron')
 
 const eyeNotification = {
-    title: 'Blink Alert',
-    body: 'Look away from your screen and Blink!'
+  title: "Blink Alert",
+  body: "Look away from your screen and Blink!",
+};
+
+const breakNotification = {
+  title: "Time for a break!",
+  body: "Take a short walk/ sip some water!",
+};
+
+const inactiveNotification = {
+  title: "Still There??",
+  body: "Hey, you were probably idle for the last 1/2 hr. Take a break!",
+};
+
+const welcomeNotification = {
+  title: "We're on it!!",
+  body: "Hello! You'll be notified soon!",
+};
+
+var activeTime = { value: 0 };
+var inActiveTime = { value: 0 };
+var active = { state: false };
+
+function updateActiveTime() {
+  if (active.state) {
+    activeTime.value += 5;
+    inActiveTime.value = 0;
+  } else {
+    inActiveTime.value += 5;
+    if (inActiveTime.value >= 10) activeTime.value = 0;
   }
- 
-  const breakNotification = {
-    title: 'Time for a break!',
-    body: 'Take a short walk/ sip some water!'
+  console.log("active " + activeTime.value + " inactive " + inActiveTime.value);
+  //ioHook.removeAllListeners(['mousemove', 'keydown']);
+
+  if (activeTime.value === 20 || activeTime.value === 40) {
+    new window.Notification(eyeNotification.title, eyeNotification);
+  } else if (activeTime.value === 60) {
+    new window.Notification(breakNotification.title, breakNotification);
+    activeTime.value = 0;
+  } else if (inActiveTime.value === 30) {
+    new window.Notification(inactiveNotification.title, inactiveNotification);
+    inActiveTime.value = 0;
   }
-
-var activeTime = {value:0};
-var active = {state: false};
-
-
-function updateActiveTime(){
-    console.log("updating "+ activeTime.value)
-    if(active.state){
-        activeTime.value += 5;
-    }
-    else{
-        activeTime.value = 0;
-    }
-    console.log("updated "+ activeTime.value)
-    //ioHook.removeAllListeners(['mousemove', 'keydown']); 
-    
-    if(activeTime.value === 20 || activeTime.value === 40)
-    {
-        const myNotification = new window.Notification(eyeNotification.title, eyeNotification)
-    }
-
-    else if(activeTime.value === 60){
-        //alert("big break");
-        const newNotification = new window.Notification(breakNotification.title, breakNotification)
-        activeTime.value = 0;
-    }
+  active.state = false;
 }
 
-ioHook.on('mousemove', event => {
-    active.state = true;
-    console.log("mosue");
+ioHook.on("mousemove", (event) => {
+  active.state = true;
+  console.log("mosue");
 });
 
-ioHook.on('keydown', event => {
-    active.state = true;
-    console.log("keyb");
- });  
+ioHook.on("keydown", (event) => {
+  active.state = true;
+  console.log("keyb");
+});
 
-function calculateActiveTime() {
-    console.log("inside calc func"+ active.state);
-    active.state = false;
+startBtn.addEventListener("click", function (event) {
+  var currentwindow = remote.getCurrentWindow();
+  currentwindow.minimize();
 
+  // Register and start hook
+  ioHook.start();
+  new window.Notification(welcomeNotification.title, welcomeNotification);
 
-      //alert("active is "+ active.state)
-    console.log("inside calc updated"+ active.state);
-    setTimeout(updateActiveTime,4990);
-}
-
-startBtn.addEventListener('click', function (event) {
-    // var homewindow = remote.getCurrentWindow();
-    // homewindow.minimize();
-
-    // Register and start hook  
-    ioHook.start();
-    
-    calculateActiveTime();
-    setInterval ( calculateActiveTime, 5000 );
-})
+  // start tracking..
+  updateActiveTime();
+  setInterval(updateActiveTime, 5000);
+});
--- src/index.js
+++ src/index.js
@@ -1,34 +1,36 @@
-const electron = require('electron')
-const path = require('path')
-const remote = electron.remote
-const BrowserWindow = electron.remote.BrowserWindow
+const electron = require("electron");
+const path = require("path");
+const remote = electron.remote;
+const BrowserWindow = electron.remote.BrowserWindow;
 
-const beginBtn = document.getElementById('beginbtn')
+const beginBtn = document.getElementById("beginbtn");
 
-beginBtn.addEventListener('click', function (event) {
-    const modalPath = path.join('file://', __dirname, 'configure.html')
-    
-    var homewindow = remote.getCurrentWindow();
+beginBtn.addEventListener("click", function (event) {
+  const modalPath = path.join("file://", __dirname, "configure.html");
 
-    let confwin = new BrowserWindow(
-        { //alwaysOnTop: true, 
-          width: 600,
-          height: 500 ,
-          minimizable: false,
-          maximizable: false,
-          webPreferences: {
-            nodeIntegration: true
-          }
-        })
-    confwin.on('close', function () { confwin = null })
-    confwin.loadURL(modalPath)
-    confwin.show()
-    //confwin.webContents.openDevTools()
+  var homewindow = remote.getCurrentWindow();
+
+  let confwin = new BrowserWindow({
+    //alwaysOnTop: true,
+    width: 600,
+    height: 500,
+    minimizable: true,
+    maximizable: true,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  confwin.on("close", function () {
+    confwin = null;
+  });
+  confwin.loadURL(modalPath);
+  confwin.show();
+  //confwin.webContents.openDevTools();
+
   //   confwin.on('minimize',function(event){
   //     event.preventDefault()
   //     confwin.focus()
   // })
 
-    homewindow.close();
-
-})
+  homewindow.close();
+});
